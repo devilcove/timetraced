522947b211342c9d268a2ba69d30cea4fde21c7e
Matthew R Kasun <mkasun@nusak.ca>
2023-12-05 08:39:46 -0500

    error handling

--- models/errors.go
+++ models/errors.go
@@ -1,5 +1,19 @@
 package models
 
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type ErrorMessage struct {
+	Status  int
 	Message string
+}
+
+func (e *ErrorMessage) Process(c *gin.Context) {
+	slog.Error(e.Message, "status", e.Status)
+	c.HTML(http.StatusOK, "error", e)
+	c.Abort()
 }
